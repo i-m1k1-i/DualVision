c4ed99528e7be68589f3a3620363f27c33575820
Mk <belmirbek7@gmail.com>
2025-01-09 03:47:19 +0600

    AirFollower and EnemyShooter are created and done. EnemySpawner and FlyingShooter prefabs are made.

---
+++ Assets/Scripts/Enemy/AirFollower.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+class AirFollower : Moveable
+{
+    [SerializeField] private float _flyHeight;
+    [SerializeField] private float _speed;
+
+    private void Start()
+    {
+        transform.position = new Vector3(transform.position.x, _flyHeight, transform.position.z);
+    }
+
+    public override void Move()
+    {
+        Vector3 direction = (Player.Instance.transform.position - transform.position).normalized;
+        direction.y = 0;
+        transform.Translate(_speed * Time.deltaTime * direction);
+    }
+}
--- Assets/Scripts/Enemy/ShootingEnemy.cs
+++ Assets/Scripts/Enemy/ShootingEnemy.cs
@@ -2,15 +2,47 @@ using UnityEngine;
 
 public class ShootingEnemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Moveable _moveable;
+
+    [SerializeField] private float _shootDistance;
+    [SerializeField] private float _shootDelay;
+    [SerializeField] private float _shootDamage;
+
+    private float _shootTime;
+    private bool _stop;
+
+    private void Update()
     {
-        
+        if (_stop == false)
+            _moveable.Move();
+
+        float distanceToPlayer = Vector3.Distance(transform.position, Player.Instance.transform.position);
+        if (distanceToPlayer <= _shootDistance)
+        {
+            _stop = true;
+        }
+        else
+        {
+            _stop = false;
+        }
+
+        if (_shootTime >= _shootDelay && _stop)
+        {
+            Shoot();
+        }
+        else
+        {
+            _shootTime += Time.deltaTime;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Shoot()
     {
-        
+       Vector3 directionToPlayer = (Player.Instance.transform.position - transform.position).normalized;
+        EnemyBullet bullet = EnemyBulletPool.Instance.Get();
+        bullet.transform.position = transform.position;
+        bullet.Direction = directionToPlayer;
+
+        _shootTime = 0;
     }
 }
