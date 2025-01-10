ae0b2a22cc5c6949060be3cb6a14ad983f236f63
m1k1 <belmirbek7@gmail.com>
2025-01-11 00:07:31 +0600

    Enemy kill counter added

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -2,4 +2,17 @@ using UnityEngine;
 
 public abstract class Enemy : MonoBehaviour
 {
+    protected static int _killedEnemies;
+    public static int KilledEnemies => _killedEnemies;
+
+    protected virtual void Awake()
+    {
+        EnemyHealth health = GetComponent<EnemyHealth>();
+        health.OnDeath += CountDie;
+    }
+
+    private void CountDie()
+    {
+        _killedEnemies++;
+    }
 }
