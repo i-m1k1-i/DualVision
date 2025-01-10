e9c63f434af07c4199baa80056b03b80d9bbaf11
m1k1 <belmirbek7@gmail.com>
2025-01-11 03:12:34 +0600

    Minimap added

--- Assets/Scripts/Player/CameraManager.cs
+++ Assets/Scripts/Player/CameraManager.cs
@@ -1,11 +1,16 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CameraManager : MonoBehaviour
 {
     [SerializeField] private Camera _sideCamera;
     [SerializeField] private Camera _topCamera;
+    [SerializeField] private RawImage _sideView;
+    [SerializeField] private RawImage _topView;
 
     private bool _isSideCamera = true;
+
+    public Camera CurrentCamera => _isSideCamera ? _sideCamera : _topCamera;
 
     private void Awake()
     {
@@ -21,6 +26,9 @@ public class CameraManager : MonoBehaviour
     private void UpdateActiveCamera()
     {
         _sideCamera.gameObject.SetActive(_isSideCamera);
+        _topView.gameObject.SetActive(_isSideCamera);
+
         _topCamera.gameObject.SetActive(!_isSideCamera);
+        _sideView.gameObject.SetActive(!_isSideCamera);
     }
 }
