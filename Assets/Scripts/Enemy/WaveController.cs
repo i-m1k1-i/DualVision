6fce3a98494d3434299438654fe2dbc81fb6e663
m1k1 <belmirbek7@gmail.com>
2025-01-12 19:25:43 +0600

    EnemySpawner separated to EnemySpawner and WaveControler

---
+++ Assets/Scripts/Enemy/WaveController.cs
@@ -0,0 +1,70 @@
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class WaveController : MonoBehaviour
+{
+    [Header("Components")]
+    [SerializeField] private NewWaveAlert _alert;
+
+    [Header("Wave settings")]
+    [SerializeField] private float _spawnInterval;
+    [SerializeField] private float _newWaveInterval;
+
+    private EnemySpawner _spawner;
+    private float _lastSpawnTime;
+    private float _lastWaveStartTime;
+    private bool _isProcessingNewWave;
+    private bool _spawning = true;
+    private int _wave = 1;
+
+    private void Start()
+    {
+        _spawner = GetComponent<EnemySpawner>();
+    }
+
+    private void Update()
+    {
+        if (_isProcessingNewWave)
+            return;
+
+
+        if (IsTimePassed(_lastSpawnTime, _spawnInterval) && _spawning)
+        {
+            _spawner.Spawn();
+            _lastSpawnTime = PlayTime.Current;
+        }
+
+        if (IsTimePassed(_lastWaveStartTime, _newWaveInterval))
+        {
+            _spawning = false;
+            if (Enemy.LiveEnemies == 0)
+            {
+                StartNewWave();
+            }
+        }
+    }
+
+    private async void StartNewWave()
+    {
+        _isProcessingNewWave = true;
+        await NewWave();
+        _isProcessingNewWave = false;
+    }
+
+    private async Task NewWave()
+    {
+        _wave++;
+        await _alert.StartAlert(_wave);
+        _spawning = true;
+        _lastWaveStartTime = PlayTime.Current;
+        if (_wave <= 6)
+        {
+            _spawnInterval *= 0.7f;
+        }
+    }
+
+    private bool IsTimePassed(float since, float interval)
+    {
+        return PlayTime.Current - since >= interval;
+    }
+}
