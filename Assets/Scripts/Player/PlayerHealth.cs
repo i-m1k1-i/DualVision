f2b7941ebe0fa669f354b9709e4169f7c34c98fb
m1k1 <belmirbek7@gmail.com>
2025-01-14 02:47:09 +0600

    Refactor, sfx

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,7 +1,26 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayerHealth : Health
 {
+    public event UnityAction OnDeath;
+
+    public override void TakeDamage(float damage)
+    {
+        if (_currentHealth <= 0)
+            return;
+
+        SoundEffectsManager.Instance.PlayPlayerHitSound();
+
+        _currentHealth = Mathf.Clamp(_currentHealth - damage, 0, _maxHealth);
+
+        if (_currentHealth <= 0)
+        {
+            Death();
+            OnDeath?.Invoke();
+        }
+    }
+
     protected override void Death()
     {
         gameObject.SetActive(false);
