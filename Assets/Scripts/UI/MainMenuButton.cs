7cd6da638b95e7ff3439839ced7273a90c6713f3
m1k1 <belmirbek7@gmail.com>
2025-01-14 19:14:17 +0600

    Pausing added

---
+++ Assets/Scripts/UI/MainMenuButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts.UI
+{
+    public class MainMenuButton : UIButton
+    {
+        private const string MainMenu = nameof(MainMenu);
+
+        protected override void Start()
+        {
+            base.Start();
+
+            _button.onClick.AddListener(LoadMainMenu);
+        }
+
+        private void LoadMainMenu()
+        {
+            SceneManager.LoadScene(MainMenu);
+        }
+    }
+}
